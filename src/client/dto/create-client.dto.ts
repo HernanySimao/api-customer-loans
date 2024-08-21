6118793dcb18e4118bc4c3d98b626bd5e859cf22
Hernany Simao <hernanysimao123@gmail.com>
2024-08-21 17:51:10 +0100

    dto and entities done

--- src/client/dto/create-client.dto.ts
+++ src/client/dto/create-client.dto.ts
@@ -1,1 +1,20 @@
-export class CreateClientDto {}
+import { IsNotEmpty, IsNumber, IsString } from "class-validator"
+
+export class CreateClientDto {
+    @IsNumber()
+    @IsNotEmpty()
+    age: number
+
+    @IsString()
+    @IsNotEmpty()
+    cpf: string
+
+    @IsString()
+    name: string
+
+    @IsNumber()
+    income: number
+
+    @IsString()
+    location: string
+}
--- src/client/entities/client.entity.ts
+++ src/client/entities/client.entity.ts
@@ -1,1 +1,9 @@
-export class Client {}
+export class Client {
+    id: number;
+    age: number;
+    cpf: string;
+    name: string;
+    income: number;
+    location: string;
+    createAt: Date;
+}
