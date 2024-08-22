7dede3948534c35e6fb2dd6cc7cd5f723bd24994
Hernany Simao <hernanysimao123@gmail.com>
2024-08-22 02:00:42 +0100

    Cors

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,9 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: '*'
+  })
   app.useGlobalPipes(new ValidationPipe({
     whitelist: true,
     forbidNonWhitelisted: true,
